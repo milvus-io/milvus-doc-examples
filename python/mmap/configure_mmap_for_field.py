5b5d164ed008adadec8c5223d41f39b4ffa696db
groot <yihua.mo@zilliz.com>
2025-03-07 18:16:09 +0800

    Add example of mmap (#12)
    Signed-off-by: yhmo <yihua.mo@zilliz.com>

---
+++ python/mmap/configure_mmap_for_field.py
@@ -0,0 +1,33 @@
+from pymilvus import MilvusClient, DataType
+
+CLUSTER_ENDPOINT="http://localhost:19530"
+TOKEN="root:Milvus"
+
+client = MilvusClient(
+    uri=CLUSTER_ENDPOINT,
+    token=TOKEN
+)
+
+schema = MilvusClient.create_schema()
+schema.add_field("id", DataType.INT64, is_primary=True, auto_id=False)
+schema.add_field("vector", DataType.FLOAT_VECTOR, dim=5)
+
+# Disable mmap on a field upon creating the schema for a collection
+schema.add_field(
+    field_name="doc_chunk",
+    datatype=DataType.INT64,
+    max_length=512,
+    # highlight-next-line
+    mmap_enabled=False,
+)
+
+client.drop_collection(collection_name="my_collection")
+client.create_collection(collection_name="my_collection", schema=schema)
+
+# Disable mmap on an existing field
+# The following assumes that you have a collection named `my_collection`
+client.alter_collection_field(
+    collection_name="my_collection",
+    field_name="doc_chunk",
+    field_params={"mmap.enabled": True}
+)
